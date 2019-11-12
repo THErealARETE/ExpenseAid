3ba1bf5de07ae6af49537bdf132a42b72c15e17b
Amuzu Imuse <Arete>
2019-11-12 17:58:12 +0100

    fixed the express server

--- server/server.js
+++ server/server.js
@@ -2,14 +2,21 @@ const path = require ('path')
 
 const express = require('express')
 const app = express()
-const publicPath = path.join (__dirname, '..', 'public')
+// const publicPath = path.join (__dirname, '..', 'public')
 const port = process.env.PORT ||3000
 
-app.use(express.static(publicPath))
+app.use( express.static( `${__dirname}/../build` ) );
 
-app.get('*', (req, res)=>{
-res.sendfile(path.join(publicPath, 'index.html'))
-})
+// app.use(express.static(publicPath))
+
+// app.get('*', (req, res)=>{
+// res.sendfile(path.join(publicPath, 'index.html'))
+// })
+
+// const path = require('path')
+app.get('*', (req, res)=>
+{  res.sendFile(path.join(__dirname, '../build/index.html'));})
+
 app.listen(port, (()=>{
     console.log('server is up')
 }) )
